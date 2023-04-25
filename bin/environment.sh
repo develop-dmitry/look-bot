e24f53ed8c635af09d95fb7fb4bb4a4e56260122
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-04-25 19:16:05 +0300

    Фикс окружения

--- bin/environment.sh
+++ bin/environment.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo -u www-data cp .env.example .env
-sudo -u www-data sed -i -- "s|%APP_URL%|$1|g" .env
-sudo -u www-data sed -i -- "s|%DB_HOST%|$2|g" .env
-sudo -u www-data sed -i -- "s|%DB_PORT%|$3|g" .env
-sudo -u www-data sed -i -- "s|%DB_NAME%|$4|g" .env
-sudo -u www-data sed -i -- "s|%DB_USER%|$5|g" .env
-sudo -u www-data sed -i -- "s|%DB_PASS%|$6|g" .env
-sudo -u www-data sed -i -- "s|%REDIS_HOST%|$7|g" .env
-sudo -u www-data sed -i -- "s|%REDIS_PASS%|$8|g" .env
-sudo -u www-data sed -i -- "s|%REDIS_PORT%|$9|g" .env
-sudo -u www-data sed -i -- "s|%TELEGRAM_BOT_TOKEN%|${10}|g" .env
-sudo -u www-data sed -i -- "s|%WWWUSER%|$UID|g" .env
-sudo -u www-data sed -i -- "s|%WWWGROUP%|$UID|g" .env
+cp .env.example .env
+sed -i -- "s|%APP_URL%|$1|g" .env
+sed -i -- "s|%DB_HOST%|$2|g" .env
+sed -i -- "s|%DB_PORT%|$3|g" .env
+sed -i -- "s|%DB_NAME%|$4|g" .env
+sed -i -- "s|%DB_USER%|$5|g" .env
+sed -i -- "s|%DB_PASS%|$6|g" .env
+sed -i -- "s|%REDIS_HOST%|$7|g" .env
+sed -i -- "s|%REDIS_PASS%|$8|g" .env
+sed -i -- "s|%REDIS_PORT%|$9|g" .env
+sed -i -- "s|%TELEGRAM_BOT_TOKEN%|${10}|g" .env
+sed -i -- "s|%WWWUSER%|${11}|g" .env
+sed -i -- "s|%WWWGROUP%|${12}|g" .env
