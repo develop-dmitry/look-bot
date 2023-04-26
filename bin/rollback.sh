683471990d4ed1bdc8716154985691f4fa7a94ee
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-04-26 20:03:10 +0300

    Rollback

---
+++ bin/rollback.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo cp deploy/nginx.conf /etc/nginx/conf.d/look-bot.conf -f
+sudo sed -i -- "s|%SERVER_NAME%|$1|g" /etc/nginx/conf.d/look-bot.conf
+sudo systemctl restart nginx
+sudo systemctl restart php8.1-fpm restart
+
+sudo -u www-data php artisan cache:clear
+sudo -u www-data php artisan config:clear
+sudo -u www-data php artisan config:cache
+sudo -u www-data php artisan route:clear
+sudo -u www-data php artisan route:cache
