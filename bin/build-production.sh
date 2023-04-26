a98db2c9bddfd092feaadd6b840244797260567f
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-04-26 21:04:56 +0300

    Deploy

--- bin/build-production.sh
+++ bin/build-production.sh
@@ -10,7 +10,5 @@ sudo systemctl restart php8.1-fpm
 
 sudo -u www-data php artisan cache:clear
 sudo -u www-data php artisan config:clear
-sudo -u www-data php artisan config:cache
 sudo -u www-data php artisan route:clear
-sudo -u www-data php artisan route:cache
 sudo -u www-data php artisan migrate --force
