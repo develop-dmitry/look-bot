71d1c6598c2394879aff68c129b80e23e1cace71
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-04-26 20:55:59 +0300

    Deploy

--- bin/build-production.sh
+++ bin/build-production.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo rm -rf storage
+
+sudo -u www-data ln -s /var/www/lookbot/shared/.env .env
+sudo -u www-data ln -s /var/www/lookbot/shared/storage storage
+
 sudo -u www-data composer install --ignore-platform-reqs
 sudo systemctl restart php8.1-fpm
 
