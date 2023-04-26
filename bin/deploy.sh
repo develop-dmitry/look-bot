15ae2e80cc12e2a42d2abe679b03fe4f4fae0f04
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-04-26 19:44:45 +0300

    Деплой

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo cp deploy/nginx.conf /etc/nginx/conf.d/look-bot.conf -f
-sudo sed -i -- "s|%SERVER_NAME%|$1}|g" /etc/nginx/conf.d/look-bot.conf
+sudo sed -i -- "s|%SERVER_NAME%|$1|g" /etc/nginx/conf.d/look-bot.conf
 sudo systemctl restart nginx
