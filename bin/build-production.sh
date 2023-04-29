d9b28c61125cadc5613cbca7676346a48c6f305e
Dmitry Kozlov <d.kozlov@intensa.ru>
2023-04-29 21:27:04 +0300

    Фикс деплоя

--- bin/build-production.sh
+++ bin/build-production.sh
@@ -9,4 +9,5 @@ sudo -u www-data composer install --ignore-platform-reqs
 sudo systemctl restart php8.1-fpm
 
 sudo -u www-data php artisan optimize:clear
+sudo -u www-data php artisan storage:link
 sudo -u www-data php artisan migrate --force
